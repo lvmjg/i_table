8b302b9b7e170a3ce3ba81475794d4f246e2a552
Jan <63665194+lvmjg@users.noreply.github.com>
2023-06-24 19:07:06 +0200

    Add element id on the bottom of Panorama page

--- lib/features/panorama/presentation/widgets/panorama_page/panorama_page.dart
+++ lib/features/panorama/presentation/widgets/panorama_page/panorama_page.dart
@@ -14,7 +14,9 @@ class PanoramaPage extends StatefulWidget {
   final String restaurantId;
   final String elementId;
 
-  const PanoramaPage({Key? key, required this.restaurantId, required this.elementId}) : super(key: key);
+  const PanoramaPage(
+      {Key? key, required this.restaurantId, required this.elementId})
+      : super(key: key);
 
   @override
   State<PanoramaPage> createState() => _PanoramaPageState();
@@ -28,18 +30,32 @@ class _PanoramaPageState extends State<PanoramaPage> {
         appBar: RestaurantPlanAppBar(),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
         floatingActionButton: PanoramaFloatingActionButton(),
-        body: PanoramaBody(restaurantId: widget.restaurantId, elementId: widget.elementId));
+        bottomNavigationBar: Container(
+          height: 50,
+          decoration: BoxDecoration(color: Color(primary),
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(padding), topRight: Radius.circular(padding))
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                widget.elementId,
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+              SizedBox(width: padding,),
+              Icon(Icons.location_searching_rounded, color: Colors.white,)
+            ],
+          ),
+        ),
+        body: PanoramaBody(
+            restaurantId: widget.restaurantId, elementId: widget.elementId));
   }
 
   @override
   void initState() {
     super.initState();
-    context.read<PanoramaBloc>().add(
-        PanoramaInit(restaurantId: widget.restaurantId, elementId: widget.elementId));
-
+    context.read<PanoramaBloc>().add(PanoramaInit(
+        restaurantId: widget.restaurantId, elementId: widget.elementId));
   }
 }
-
-
-
-
