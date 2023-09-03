63aac40c2c836eb85a6289d33aa545135ea14b65
Jan <63665194+lvmjg@users.noreply.github.com>
2023-09-03 08:51:54 +0200

    Update globals.dart

--- lib/core/util/globals.dart
+++ lib/core/util/globals.dart
@@ -10,6 +10,8 @@ double padding =16.0;
 double elevation = 20;
 double toolbarHeight = 70;
 
+String loggedUserId = 'TestUser';
+
 String appName = 'iTable';
 String searchBarHint = 'Szukaj punktu...';
 String noResults = 'Brak wyników';
@@ -18,27 +20,44 @@ String choosePlace = 'Wybierz punkt, aby dokonać rezerwacji';
 String reserve = 'Rezerwuj';
 String chooseDate = 'Wybierz termin';
 String chooseTime = 'Wybierz godzinę';
+String chooseDuration = 'Wybierz czas trwania';
 String closePreview = 'Zamknij podgląd';
+String choosePlaces = 'Wybierz miejsca';
+String myRservations = 'Moje rezerwacje';
+String remove = 'Usuń';
+String add = 'Dodaj';
 
 String imageExtension = '.jpg';
 
+String errorFetchPlaceMenu = 'Nie można pobrać menu';
+String errorFetchUserReservations = 'Nie można pobrać listy rezerwacji';
 String errorFetchPlaces = 'Nie można pobrać listy punktów';
 String errorFetchPlaceDetails = 'Nie można pobrać informacji o punkcie';
 String errorShowPreview = 'Nie można wyświetlić podglądu miejsca';
 String errorFetchPlace = 'Nie załadować poglądu punktu';
+String errorFetchPlaceSettings = 'Nie można ustawień punktu';
 String tryAgain = 'Spróbuj ponownie';
 
 String pathPlaceId = 'placeId';
+String pathUserId = 'userId';
 String pathPlaces = 'places';
 String pathPlacesDetails = 'placesDetails';
 String pathPlacesSettings = 'placesSettings';
 String pathPlacesPlans = 'placesPlans';
 String pathPlacePlanLevels = 'placePlanLevels';
 String pathPlacesReservations = 'placesReservations';
-String pathReservationStart = 'reservationStart';
-String pathReservationEnd= 'reservationEnd';
+String pathStartDate = 'startDate';
 String pathPanoramas = 'panoramas';
+String pathPlaceMenu = 'placeMenu';
 
+String errorPickedDateAfter = "Rezerwację można złożyć na maksymalnie %d dni wcześniej";
+String errorPickedDateBefore = "Nie można złożyć rezerwacji z terminem w przeszłości";
+String placeNotWorkingHours = "Punkt nie pracuje w wybranej godzinie";
+String placeClosed = 'Nieczynne';
+
+String ok = 'OK';
+
+String menu = 'Menu';
 
 const RoundedRectangleBorder roundedRectangleBorder = RoundedRectangleBorder(
 
