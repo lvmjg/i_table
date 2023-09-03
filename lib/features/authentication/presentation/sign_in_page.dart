854b267a97e4d7c7b96a609822bd31fd7014f0a3
Jan <63665194+lvmjg@users.noreply.github.com>
2023-09-03 09:16:32 +0200

    SignInPage (only UI)

--- lib/features/authentication/presentation/sign_in_page.dart
+++ lib/features/authentication/presentation/sign_in_page.dart
@@ -2,8 +2,10 @@ import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:i_table/features/place_search/presentation/widgets/search_page/place_search_page.dart';
 
 import '../../../../../core/util/globals.dart';
+import '../../home/presentation/widgets/home_page/home_page.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({
@@ -13,70 +15,71 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion(
-      value: Theme.of(context).appBarTheme.systemOverlayStyle!,
-      child: Container(
-      //  color: Color(primary),
-        child: Theme(
-          data: ThemeData(
-              colorScheme: ColorScheme.light(primary: Colors.white60, secondary: Colors.white),
-              scaffoldBackgroundColor: Color(primary),
-            textTheme: GoogleFonts.signikaNegativeTextTheme(Theme.of(context).textTheme).apply(displayColor: Colors.white, bodyColor: Colors.white),
+      value: SystemUiOverlayStyle(
+        statusBarColor: Color(primary),
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Colors.black,
+        systemNavigationBarIconBrightness: Brightness.light,
+      ),
+      child: Theme(
+        data: ThemeData(
+          colorScheme: ColorScheme.light(primary: Colors.grey),
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: GoogleFonts.signikaNegativeTextTheme(
+                  Theme.of(context).textTheme)
 
-            inputDecorationTheme: InputDecorationTheme(
-              floatingLabelBehavior: FloatingLabelBehavior.never,
-              suffixStyle:TextStyle(color: Colors.black) ,
-              helperStyle: TextStyle(color: Colors.black),
-              prefixStyle: TextStyle(color: Colors.black),
-              hintStyle:TextStyle(color: Colors.black) ,
-              labelStyle: TextStyle(color: Colors.black),
-              fillColor: Colors.white,
-              filled: true,
-              border: OutlineInputBorder(
+              .apply(),
+          inputDecorationTheme: InputDecorationTheme(
+
+            floatingLabelBehavior: FloatingLabelBehavior.never,
+            fillColor: Colors.white,
+            filled: true,
+            enabledBorder:OutlineInputBorder(
                 borderRadius: BorderRadius.circular(200),
-                borderSide: BorderSide.none
+                borderSide: BorderSide(color: Colors.grey, width: 1)),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(200),
+            borderSide: BorderSide(color: Color(primary), width: 1)),
+          ),
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: ButtonStyle(
+              padding: MaterialStateProperty.all<EdgeInsets>(
+                EdgeInsets.all(padding),
               ),
-
-            ),
-            outlinedButtonTheme: OutlinedButtonThemeData(
-              style: ButtonStyle(
-                padding: MaterialStateProperty.all<EdgeInsets>(
-                  EdgeInsets.all(padding),
-                ),
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                foregroundColor: MaterialStateProperty.all<Color>(Color(primary)),
-              ),
+              backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+              foregroundColor:
+                  MaterialStateProperty.all<Color>(Color(primary)),
             ),
           ),
-          child: SafeArea(
-
-            child: SignInScreen(
-              headerBuilder: (context, contraints, test){
-                return Container(
-                  decoration: BoxDecoration(color: Colors.white,
-
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(padding), bottomRight: Radius.circular(padding))
+        ),
+        child: SafeArea(
+          child: SignInScreen(
+            headerBuilder: (context, contraints, test) {
+              return Container(
+                decoration: BoxDecoration(
+                    color: Color(primary),
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(padding),
+                        bottomRight: Radius.circular(padding))),
+                height: 100,
+                child: Center(
+                  child: Text(
+                    appName,
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w900),
                   ),
-
-                  height: 100,
-                  child: Center(
-                    child:Text(
-                      appName,
-                      style: TextStyle(
-                          fontSize: 24,
-                          color: Color(primary),
-                          fontWeight: FontWeight.w900),
-                    ),
-                  ),
-                );
-              },
-
-
-             // styles: FirebaseUIStyle.ofType(),
-              //  styles: ,
-
-              providers: [EmailAuthProvider(), UniversalEmailSignInProvider()],
-
-            ),
+                ),
+              );
+            },
+            actions: [
+              AuthStateChangeAction((context, state) {
+                Navigator.of(context).pushReplacement(MaterialPageRoute(
+                    builder: (context) => HomePage()));
+              })
+            ],
+            providers: [EmailAuthProvider(), UniversalEmailSignInProvider()],
           ),
         ),
       ),
